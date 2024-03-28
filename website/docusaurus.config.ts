7b73579208058bb633b8f35f74b8445265da3909
Joao Eduardo Luis <joao@1e3ms.io>
2024-03-28 11:58:44 +0000

    website: add s3gw.tech social card
    Signed-off-by: Joao Eduardo Luis <joao@1e3ms.io>

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -87,7 +87,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: "img/docusaurus-social-card.jpg",
+    image: "img/s3gw-tech-social-card.png",
     navbar: {
       title: "S3GW",
       logo: {
